36fe59a342bfb5c27831ecabba4bedbe518d8751
eric <tran.eric6@gmail.com>
2021-04-09 21:42:15 -0400

    added react bootsrap, updated recipe-viewer + home page
    added react bootsrap, updated home page to have nav, updated recipe-viewer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import  RecipeViewer from './components/recipe-viewer'
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <RecipeViewer/>
-  );
-}
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import './App.css';
+import RecipeViewer from './components/recipe-viewer';
+import { Form } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+
+
+const App = () => (
+  <Container className="p-3">
+    <Jumbotron>
+      <h1 className="header">Kupika</h1>
+      <div className="nav">
+        <Breadcrumb>
+          <Breadcrumb.Item href="#">Recipes</Breadcrumb.Item>
+          <Breadcrumb.Item href="#">Create a Recipe</Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+      <div className="search">
+        <Form inline>
+          <FormControl type="text" placeholder="Search" />
+          <Button type="submit">Submit</Button>
+        </Form>
+      </div>
+    </Jumbotron>
+    <RecipeViewer></RecipeViewer>
+  </Container>
+);
+
 
 export default App;
