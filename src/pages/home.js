5a269ed0e9c325c124cb8e38318d67f5e26f73ec
eric <tran.eric6@gmail.com>
2021-04-10 19:07:33 -0400

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,8 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
 import { Form, FormControl } from 'react-bootstrap';
+import { CardColumns } from 'react-bootstrap';
+
 import Header from '../components/header';
 import RecipePage from "../pages/recipes";
 
